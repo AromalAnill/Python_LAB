aa0a3aa45a7c5063845504bd232478606a01a05a
AromalAnill <aaromalanill@gmail.com>
2025-10-09 14:56:01 +0530

    Add files via upload

---
+++ multiple.py
@@ -0,0 +1,20 @@
+n=int(input("enter the number for multiple table:"))
+limit=int(input("enter the limit for multiple table:"))
+for i in range(1,limit+1):
+    print(n,"x",i,"=",n*i)
+
+
+'''
+enter the number for multiple table: 5
+enter the limit for multiple table:10
+5 x 1 = 5
+5 x 2 = 10
+5 x 3 = 15
+5 x 4 = 20
+5 x 5 = 25
+5 x 6 = 30
+5 x 7 = 35
+5 x 8 = 40
+5 x 9 = 45
+5 x 10 = 50
+'''
