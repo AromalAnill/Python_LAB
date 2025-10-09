61868cfd833768082730cf92fa99d1001f3e16b0
AromalAnill <aaromalanill@gmail.com>
2025-10-09 14:36:21 +0530

    Add files via upload

---
+++ basic_pay.py
@@ -0,0 +1,17 @@
+basic_pay=float(input("Enter your basic pay: "))
+hra=0.10*basic_pay
+ta=0.05*basic_pay
+salary=basic_pay+hra+ta
+print("basic pay :",basic_pay)
+print("hra :",hra)
+print("ta :",ta)
+print("salary of employee:",salary)
+
+'''
+Enter your basic pay: 100
+basic pay : 100.0
+hra : 10.0
+ta : 5.0
+salary of employee: 115.0
+'''
+a=5
---
+++ largest_3.py
@@ -0,0 +1,17 @@
+n1=int(input("Enter the first number "))
+n2=int(input("Enter the second number"))
+n3=int(input("Enter the third number"))
+if n1>n2 and n1>n3:
+    print("largest no is",n1)
+elif n2>n3 and n2>n1:
+  print("largest no is",n2)
+else:
+    print("largest no is",n3)
+
+    '''
+      
+    Enter the first number 50
+    Enter the second number60
+    Enter the third number30
+    largest no is 60
+    '''
