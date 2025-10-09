7688c4e24dbae3f62cd6e24a02e7b31357707dd8
AromalAnill <aaromalanill@gmail.com>
2025-10-09 14:56:22 +0530

    Add files via upload

---
+++ color.py
@@ -0,0 +1,12 @@
+colors = input("Enter colors separated by comma: ").split(',')
+print("colors list:", colors)
+print("first colors:", colors [0])
+print("last colors :", colors [-1])
+
+
+'''
+Enter colors separated by comma: red,blue,black
+colors list: ['red', 'blue', 'black']
+first colors: red
+last colors : black
+'''
